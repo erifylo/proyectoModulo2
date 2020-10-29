52f4a55b55036ae66b76dc1a96eba0f40f135a04
erifylo <erif.evangelou@gmail.com>
2020-10-29 15:04:07 +0100

    layout ok y dashboard.hbs

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,8 @@ const router  = express.Router();
 /* GET home page */
 router.get('/', (req, res, next) => {
   let userLogged = req.session.currentUser !==undefined;
-  res.render('index', {"isUserLoggedIn": userLogged});
+  res.render('index', {"isUserLoggedIn": userLogged,  layout: 'layoutNotLoggedIn.hbs' });
 });
-
 
 
 module.exports = router;
